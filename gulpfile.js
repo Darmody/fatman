09845c4a26c5e03e28fb7fe9cb5a68e8e26f5574
darmody <darmody@che3bao.com>
2015-09-15 09:15:06 +0800

    添加livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     del = require('del'),
-    nodemon = require('gulp-nodemon');
+    nodemon = require('gulp-nodemon'),
+    livereload = require('gulp-livereload');
 
 function clean() {
   return del.sync(['public']);
@@ -8,15 +9,20 @@ function clean() {
 
 var publ1c = function(){
   gulp.src('./app/elements/**')
-    .pipe(gulp.dest('./public/elements/'));
+    .pipe(gulp.dest('./public/elements/'))
+    .pipe(livereload());
   gulp.src('./views/elements/**')
-    .pipe(gulp.dest('./public/elements/'));
+    .pipe(gulp.dest('./public/elements/'))
+    .pipe(livereload());
   gulp.src('./bower_components/**')
-    .pipe(gulp.dest('./public/components/'));
+    .pipe(gulp.dest('./public/components/'))
+    .pipe(livereload());
   gulp.src('./styles/**')
-    .pipe(gulp.dest('./public/styles/'));
+    .pipe(gulp.dest('./public/styles/'))
+    .pipe(livereload());
   gulp.src('./images/**')
-    .pipe(gulp.dest('./public/images/'));
+    .pipe(gulp.dest('./public/images/'))
+    .pipe(livereload());
 };
 
 gulp.task('dev', function () {
