eecb0f186212300f051479ba7c5cadff7acc50da
darmody <darmody@che3bao.com>
2015-09-16 09:12:39 +0800

    切分months-nav elements

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp'),
     del = require('del'),
     nodemon = require('gulp-nodemon'),
-    livereload = require('gulp-livereload');
+    browserSync = require('browser-sync');
 
 function clean() {
   return del.sync(['public']);
@@ -10,19 +10,14 @@ function clean() {
 var publ1c = function(){
   gulp.src('./app/elements/**')
     .pipe(gulp.dest('./public/elements/'))
-    .pipe(livereload());
   gulp.src('./views/elements/**')
     .pipe(gulp.dest('./public/elements/'))
-    .pipe(livereload());
   gulp.src('./bower_components/**')
     .pipe(gulp.dest('./public/components/'))
-    .pipe(livereload());
   gulp.src('./styles/**')
     .pipe(gulp.dest('./public/styles/'))
-    .pipe(livereload());
   gulp.src('./images/**')
     .pipe(gulp.dest('./public/images/'))
-    .pipe(livereload());
 };
 
 gulp.task('dev', function () {
